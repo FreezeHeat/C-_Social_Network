7848da54f0c9fa0f3c134f62ca5f7eab39457d1b
Asaf Y <asafyeshayahu@gmail.com>
2016-02-11 03:35:01 +0200

    Fixed Database and SQL Queries to work properly, now the login menu opens so we can advance further and fix everything that's in the way.

--- SocialNetwork/Program.cs
+++ SocialNetwork/Program.cs
@@ -31,61 +31,82 @@ namespace SocialNetwork
             Caretaker backupManager = new Caretaker(); // מנהל גיבוי
             Database database = Database.getDatabase(); // בסיס נתונים
 
-            database.Accounts.Add(new Admin
-                (
-                    "Aviram",
-                    "Aviram",
-                    "Sharabi",
-                    "1234"
-                ));
+            if (database.checkIfUserExists("Aviram") == false)
+            {
+                database.addAdmin(new Admin("Aviram", "Aviram", "Sharabi", "1234"));
+            }
+            //database.Accounts.Add(new Admin
+            //    (
+            //        "Aviram",
+            //        "Aviram",
+            //        "Sharabi",
+            //        "1234"
+            //    ));
 
-            database.Accounts.Add(new TechSupport
-            (
-                "Amar",
-                "Omer",
-                "Regev",
-                "1234"
+            if (database.checkIfUserExists("Amar") == false)
+            {
+                database.addTech(new TechSupport("Amar", "Omer", "Regev", "1234"));
+            }
 
-            ));
-            database.Accounts.Add(new User
-            (
-                "Noah",
-                "Meir",
-                "Sharabi",
-                "1234",
-                "Married with 30 Children",
-                "12/08/2014",
-                "Kefar Sava",
-                "Need more babies!",
-                "I Do Not Know Why"
-            ));
+            //database.Accounts.Add(new TechSupport
+            //(
+            //    "Amar",
+            //    "Omer",
+            //    "Regev",
+            //    "1234"
+            //));
 
-            database.Accounts.Add(new User
-            (
-                "Asaf",
-                "Asaf",
-                "Yeshayahu",
-                "1234",
-                "Married with 400 Children",
-                "01/01/1999",
-                "Head and Shoulders",
-                "It's all bullshit",
-                "Good evening!"
-            ));
+            if (database.checkIfUserExists("Noah") == false)
+            {
+                database.addUser(new User("Noah", "Meir", "Sharabi", "1234", "Married with 30 children", "12/08/2014", "Kefar Saba", "Need more babies!", "I do not know Why"));
+            }
+            //database.Accounts.Add(new User
+            //(
+            //    "Noah",
+            //    "Meir",
+            //    "Sharabi",
+            //    "1234",
+            //    "Married with 30 Children",
+            //    "12/08/2014",
+            //    "Kefar Sava",
+            //    "Need more babies!",
+            //    "I Do Not Know Why"
+            //));
 
-            database.Accounts.Add(new User
-            (
-                "Nehora",
-                "Nehora",
-                "Messika",
-                "1234",
-                "Married with 365 Children",
-                "12/12/2000",
-                "Somewhere over the rainbow",
-                "Ask",
-                "Let me sleep!"
-            ));
-            
+            if (database.checkIfUserExists("Asaf") == false)
+            {
+                database.addUser(new User("Asaf", "Asaf", "Yeshayahu", "1234", "Married with 400 children", "01/01/1999", "Head and Shoulders", "It''s all bullshit", "Good evening!"));
+            }
+            //database.Accounts.Add(new User
+            //(
+            //    "Asaf",
+            //    "Asaf",
+            //    "Yeshayahu",
+            //    "1234",
+            //    "Married with 400 Children",
+            //    "01/01/1999",
+            //    "Head and Shoulders",
+            //    "It's all bullshit",
+            //    "Good evening!"
+            //));
+
+            if (database.checkIfUserExists("Nehora") == false)
+            {
+                database.addUser(new User("Nehora", "Nehora", "Messika", "1234", "Married with 365 Children", "12/12/2000", "Somewhere over the rainbow", "Ask", "Let me sleep!"));
+            }
+            //database.Accounts.Add(new User
+            //(
+            //    "Nehora",
+            //    "Nehora",
+            //    "Messika",
+            //    "1234",
+            //    "Married with 365 Children",
+            //    "12/12/2000",
+            //    "Somewhere over the rainbow",
+            //    "Ask",
+            //    "Let me sleep!"
+            //));
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Home());
